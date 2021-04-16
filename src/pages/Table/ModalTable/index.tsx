ed924e3dc7f4c5ad74226d60dd03f3ab2589623f
lvsiyu <1030502996@qq.com>
2021-04-16 10:00:41 +0800

    feat: 新增弹框列表

--- src/pages/Table/ModalTable/index.tsx
+++ src/pages/Table/ModalTable/index.tsx
@@ -1,15 +1,29 @@
-import React, { useRef } from 'react';
-import { Space, Tooltip } from 'antd';
+import React, { useState, useRef } from 'react';
+import {
+  Button,
+  Space,
+  Tooltip,
+  Popconfirm,
+  Dropdown,
+  Menu,
+  Modal,
+  notification,
+  message,
+} from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
 import type { ActionType, ProColumns } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
 import { queryBasisTable } from './services';
 import type { BasisTableDataType } from './data';
+import { BasisModal } from './modals';
 
 const ModalsTableList: React.FC = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const ref = useRef<ActionType>();
 
-  const ProcessMap = {
+  const processMap = {
     close: 'normal',
     running: 'active',
     online: 'success',
@@ -22,10 +36,198 @@ const ModalsTableList: React.FC = () => {
     </Tooltip>
   );
 
+  const reloadTable = () => {
+    if (ref.current) ref.current.reload();
+  };
+
+  const confirm = () => {
+    message.success('点了是');
+    reloadTable();
+  };
+
+  const cancel = () => {
+    message.error('点了否');
+  };
+
   const tableAction = () => (
     <Space>
-      <a>弹框显示</a>
+      <Popconfirm
+        title="确认操作此按钮？"
+        onConfirm={confirm}
+        onCancel={cancel}
+        okText="是"
+        cancelText="否"
+      >
+        <a href="#">操作</a>
+      </Popconfirm>
     </Space>
+  );
+
+  const infoModal = () => {
+    Modal.info({
+      centered: true,
+      title: '这是消息弹出框',
+      content: (
+        <div>
+          <p>内容内容...内容内容...</p>
+          <p>内容内容...内容内容...</p>
+        </div>
+      ),
+      onOk() {
+        reloadTable();
+      },
+    });
+  };
+
+  const successModal = () => {
+    Modal.success({
+      centered: true,
+      title: '这是成功弹出框',
+      content: '内容内容...内容内容...',
+      onOk() {
+        reloadTable();
+      },
+    });
+  };
+
+  const errorModal = () => {
+    Modal.error({
+      centered: true,
+      title: '这是错误弹出框',
+      content: '内容内容...内容内容...',
+      onOk() {
+        reloadTable();
+      },
+    });
+  };
+
+  const warningModal = () => {
+    Modal.warning({
+      centered: true,
+      title: '这是警告弹出框',
+      content: '内容内容...内容内容...',
+      onOk() {
+        reloadTable();
+      },
+    });
+  };
+
+  const openNotificationWithIcon = (type: string, name: string) => {
+    notification[type]({
+      message: `这是${name}通知框`,
+      description: '内容内容...内容内容...',
+      onClose() {
+        reloadTable();
+      },
+    });
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    reloadTable();
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const modalProps = {
+    visible: isModalVisible,
+    hideModal: handleCancel,
+    handleOk,
+  };
+
+  const modalList = (
+    <Menu>
+      <Menu.Item key="1">
+        <Button key="default" onClick={showModal}>
+          默认对话框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Button type="primary" key="primary" onClick={infoModal}>
+          消息对话框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Button
+          type="primary"
+          key="primary"
+          style={{ backgroundColor: '#52C41A' }}
+          onClick={successModal}
+        >
+          成功对话框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="4">
+        <Button
+          type="dashed"
+          key="primary"
+          style={{ backgroundColor: '#FAAD14' }}
+          onClick={warningModal}
+        >
+          警告对话框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="5">
+        <Button type="primary" key="danger" danger onClick={errorModal}>
+          错误对话框
+        </Button>
+      </Menu.Item>
+    </Menu>
+  );
+
+  const notificationList = (
+    <Menu>
+      <Menu.Item key="1">
+        <Button key="default" onClick={() => openNotificationWithIcon('open', '默认')}>
+          默认通知框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Button
+          type="primary"
+          key="primary"
+          onClick={() => openNotificationWithIcon('info', '消息')}
+        >
+          消息通知框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Button
+          type="primary"
+          key="primary"
+          style={{ backgroundColor: '#52C41A' }}
+          onClick={() => openNotificationWithIcon('success', '成功')}
+        >
+          成功通知框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="4">
+        <Button
+          type="dashed"
+          key="warning"
+          style={{ backgroundColor: '#FAAD14' }}
+          onClick={() => openNotificationWithIcon('warning', '警告')}
+        >
+          警告通知框
+        </Button>
+      </Menu.Item>
+      <Menu.Item key="5">
+        <Button
+          type="primary"
+          key="danger"
+          danger
+          onClick={() => openNotificationWithIcon('error', '错误')}
+        >
+          错误通知框
+        </Button>
+      </Menu.Item>
+    </Menu>
   );
 
   const columns: ProColumns<BasisTableDataType>[] = [
@@ -69,7 +271,7 @@ const ModalsTableList: React.FC = () => {
       dataIndex: 'progress',
       valueType: (item) => ({
         type: 'progress',
-        status: ProcessMap[item.status],
+        status: processMap[item.status],
       }),
     },
     {
@@ -82,7 +284,7 @@ const ModalsTableList: React.FC = () => {
       title: '操作',
       dataIndex: 'option',
       valueType: 'option',
-      width: 75,
+      width: 46,
       render: tableAction,
     },
   ];
@@ -103,7 +305,21 @@ const ModalsTableList: React.FC = () => {
         headerTitle="弹框表格"
         request={(params) => queryBasisTable({ ...params })}
         columns={columns}
+        toolBarRender={() => [
+          <Dropdown key="menu" overlay={modalList} trigger={['click']}>
+            <Button type="primary">
+              对话框展示 <DownOutlined />
+            </Button>
+          </Dropdown>,
+          <Dropdown key="menu" overlay={notificationList} trigger={['click']}>
+            <Button type="primary">
+              通知框展示 <DownOutlined />
+            </Button>
+          </Dropdown>,
+        ]}
       />
+
+      <BasisModal {...modalProps} />
     </PageContainer>
   );
 };
