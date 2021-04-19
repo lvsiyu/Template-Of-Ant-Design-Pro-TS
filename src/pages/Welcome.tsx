caf291177d6bb5cad6509f5e4d67e1a6f9afaef0
lvsiyu <1030502996@qq.com>
2021-04-19 17:12:50 +0800

    feat: 新增欢迎页面

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,62 +1,22 @@
 import React from 'react';
+import { Card, Result, Button } from 'antd';
+import { Link } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Card, Alert, Typography } from 'antd';
-import { useIntl } from 'umi';
-import styles from './Welcome.less';
-
-const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
-);
+import cool from '@/asserts/svg/cool.svg';
 
 export default (): React.ReactNode => {
-  const intl = useIntl();
   return (
     <PageContainer>
       <Card>
-        <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: '更快更强的重型组件，已经发布。',
-          })}
-          type="success"
-          showIcon
-          banner
-          style={{
-            margin: -12,
-            marginBottom: 24,
-          }}
+        <Result
+          icon={<img src={cool} />}
+          title="欢迎使用后台管理模板"
+          extra={
+            <Button type="primary">
+              <Link to="/charts/antd-charts">开始探索</Link>
+            </Button>
+          }
         />
-        <Typography.Text strong>
-          高级表格{' '}
-          <a
-            href="https://procomponents.ant.design/components/table"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            欢迎使用
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-table</CodePreview>
-        <Typography.Text
-          strong
-          style={{
-            marginBottom: 12,
-          }}
-        >
-          高级布局{' '}
-          <a
-            href="https://procomponents.ant.design/components/layout"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            欢迎使用
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
       </Card>
     </PageContainer>
   );
