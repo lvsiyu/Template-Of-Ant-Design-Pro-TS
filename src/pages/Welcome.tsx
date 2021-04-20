fa7b3c9d6bd762830924a11ac34c9ab17a3fad2d
lvsiyu <1030502996@qq.com>
2021-04-20 10:42:37 +0800

    feat: 新增欢迎模板动态图标

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Result, Button } from 'antd';
 import { Link } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
-import cool from '@/asserts/svg/cool.svg';
+import { randomLogo } from '@/utils/emoticons';
 
 export default (): React.ReactNode => {
+  const [logoName, setLogoName] = useState(
+    randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)],
+  );
+
+  useEffect(() => {
+    setInterval(() => {
+      const name = randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)];
+      setLogoName(name);
+    }, 1000);
+  }, []);
   return (
     <PageContainer>
       <Card>
         <Result
-          icon={<img src={cool} />}
+          icon={<img src={logoName} />}
           title="欢迎使用后台管理模板"
           extra={
             <Button type="primary">
