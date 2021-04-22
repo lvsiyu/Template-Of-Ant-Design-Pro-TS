2477773463045b5b77adeea81cfd763b3bab43b1
lvsiyu <1030502996@qq.com>
2021-04-22 15:13:40 +0800

    fix: 修改欢迎页面，管理页面表情刷新

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,14 +1,14 @@
-import React /* , { useState, useEffect } */ from 'react';
-import { Card, Result } from 'antd';
+import React, { useState /* , useEffect */ } from 'react';
+import { Card, Result, Button, Space } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
 import { randomLogo } from '@/utils/emoticons';
 
 export default (): React.ReactNode => {
-  /* const [logoName, setLogoName] = useState(
+  const [logoName, setLogoName] = useState(
     randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)],
   );
 
-  useEffect(() => {
+  /* useEffect(() => {
     setInterval(() => {
       const name = randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)];
       setLogoName(name);
@@ -19,8 +19,20 @@ export default (): React.ReactNode => {
     <PageContainer>
       <Card>
         <Result
-          icon={<img src={randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)]} />}
+          icon={<img src={logoName} />}
           title="这是只有管理员才能看到的界面，普通用户登录无法看到"
+          extra={
+            <Space>
+              <Button
+                type="primary"
+                onClick={() =>
+                  setLogoName(randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)])
+                }
+              >
+                刷新表情
+              </Button>
+            </Space>
+          }
         />
       </Card>
     </PageContainer>
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,15 +1,15 @@
-import React /* , { useState, useEffect } */ from 'react';
-import { Card, Result, Button } from 'antd';
+import React, { useState } from 'react';
+import { Card, Result, Button, Space } from 'antd';
 import { Link } from 'umi';
 import { PageContainer } from '@ant-design/pro-layout';
 import { randomLogo } from '@/utils/emoticons';
 
 export default (): React.ReactNode => {
-  /* const [logoName, setLogoName] = useState(
+  const [logoName, setLogoName] = useState(
     randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)],
   );
 
-  useEffect(() => {
+  /* useEffect(() => {
     setInterval(() => {
       const name = randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)];
       setLogoName(name);
@@ -19,12 +19,22 @@ export default (): React.ReactNode => {
     <PageContainer>
       <Card>
         <Result
-          icon={<img src={randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)]} />}
+          icon={<img src={logoName} />}
           title="欢迎使用后台管理模板"
           extra={
-            <Button type="primary">
-              <Link to="/charts/antd-charts">开始探索</Link>
-            </Button>
+            <Space>
+              <Button type="primary">
+                <Link to="/charts/antd-charts">开始探索</Link>
+              </Button>
+              <Button
+                type="primary"
+                onClick={() =>
+                  setLogoName(randomLogo[parseInt(`${Math.random() * randomLogo.length}`, 10)])
+                }
+              >
+                刷新表情
+              </Button>
+            </Space>
           }
         />
       </Card>
