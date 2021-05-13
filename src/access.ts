e8b66f951084085dc0efb82aac3de646cae1f61e
lvsiyu <1030502996@qq.com>
2021-05-13 08:58:27 +0800

    fix: 修改权限判断条件

--- src/access.ts
+++ src/access.ts
@@ -4,7 +4,7 @@
 export default function access(initialState: { currentUser?: API.CurrentUser | undefined }) {
   const { currentUser } = initialState || {};
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
-    canUser: currentUser && currentUser.access === 'user',
+    canAdmin: currentUser?.access === 'admin',
+    canUser: currentUser?.access === 'user',
   };
 }
