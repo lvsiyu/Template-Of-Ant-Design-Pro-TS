d36a269123aff2a64a40d1c5bddafa79291b9aaf
lvsiyu <1030502996@qq.com>
2021-05-20 14:37:15 +0800

    fix: 修改模块引入顺序

--- src/components/HeaderSearch/index.tsx
+++ src/components/HeaderSearch/index.tsx
@@ -1,8 +1,8 @@
-import { SearchOutlined } from '@ant-design/icons';
-import { AutoComplete, Input } from 'antd';
-import useMergedState from 'rc-util/es/hooks/useMergedState';
-import type { AutoCompleteProps } from 'antd/es/auto-complete';
 import React, { useRef } from 'react';
+import useMergedState from 'rc-util/es/hooks/useMergedState';
+import { AutoComplete, Input } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import type { AutoCompleteProps } from 'antd/es/auto-complete';
 
 import classNames from 'classnames';
 import styles from './index.less';
